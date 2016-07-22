cae7e5f5026787858a72bb5d9090d8aa29409b3c
Garend95 <garend95@yahoo.com>
2016-07-23 01:02:24 +0400

    Established listening to local host with html and express, using /public to send files to clients, functions for detecting connection and disconnection of players using socket. Others for tile+player+gold bit creation and identification. Player positions at beggining of game, identifying movements and emiting to all clients alomg with player starting positions and gold bit positioning. Arrays for sockets and players.

--- index.js
+++ index.js
@@ -1,3 +1,274 @@
+var sckt = [];
+var players = [];
+
+
+var express = require('express');
+var app = express();
+
+var http = require('http').createServer(app); 
+var io = require('socket.io').listen(http);
+http.listen(8080);
+
+io.set('log level', 1);
+
+
+app.use("/public", express.static(__dirname + '/public'));
+
+app.get('/:file', function(req, res, next)
+	{	
+	
+		console.log("File is - " + req.params.file);
+	
+		res.sendfile(__dirname + '/' + req.params.file);
+	}	
+	
+	);
+
+io.sockets.on('connection', function(socket)
+{	
+	
+	console.log("The id of connected socket - " + socket.id);
+	
+	if(players.length == 4)
+		return false;
+	
+	for(var skt in io.sockets.sockets)
+	{
+		console.log(io.sockets.sockets[skt].id);
+	}
+	
+	sckt.push(socket.id);
+
+	function sideReturn(sckLength)
+		{
+			switch (sckLength)
+				{
+					case 1:
+						return "lowerLeft";
+						break;
+
+					case 2:
+						return "lowerRight"
+						break;
+
+					case 3:
+						return "upperLeft";
+						break;
+
+					case 4: 
+						return "upperRight";
+						break;
+
+					default:
+						return "false";
+						break;	
+
+
+				}
+		}
+
+	function numReturn(sckLength)
+		{
+			switch (sckLength)
+				{
+					case 1:
+						return "first";
+						break;
+
+					case 2:
+						return "second"
+						break;
+
+					case 3:
+						return "third";
+						break;
+
+					case 4: 
+						return "fourth";
+						break;
+
+					default:
+						return false;
+						break;	
+
+
+				}
+		}
+
+	function xReturn(sckLength)
+		{
+			switch (sckLength)
+				{
+					case 1:
+						return 64;
+						break;
+
+					case 2:
+						return 832;
+						break;
+
+					case 3:
+						return 64;
+						break;
+
+					case 4: 
+						return 832;
+						break;
+
+					default:
+						return false;
+						break;	
+
+
+				}
+		}
+
+	function yReturn(sckLength)
+		{
+			switch (sckLength)
+				{
+					case 1:
+						return 864;
+						break;
+
+					case 2:
+						return 864;
+						break;
+
+					case 3:
+						return 32;
+						break;
+
+					case 4: 
+						return 32;
+						break;
+
+					default:
+						return false;
+						break;	
+
+
+				}
+		}
+	
+	socket.emit("yourID", {
+							"name":socket.id,
+							"side":sideReturn(sckt.length),
+							"player":numReturn(sckt.length),
+							"x":xReturn(sckt.length),
+							"y":yReturn(sckt.length)
+						  });
+	
+	//Listening to any movement	
+	socket.on('move', function(data)
+						 {
+
+							for(var tmp in players)
+							{
+								if(players[tmp]["player"] == data["player"])
+								{
+									if(data["dir"] == "down")
+										players[tmp]["y"] += 5;
+										
+									if(data["dir"] == "up")
+										players[tmp]["y"] -= 5;
+
+									if(data["dir"] == "left")
+										player[tmp]["x"] -= 5;
+
+									if(data["dir"] == "right")
+									players[tmp]["x"] += 5;
+										
+									io.sockets.emit("someOneMove", players[tmp]);
+								}
+							}							
+						 });	
+
+	socket.on('newPlayerCreated', function(data)
+	{
+		players.push({"id":data["id"], "x":data["x"], "y":data["y"], "side":data["side"], "player":data["player"]});
+		
+		//deciding his position on the map
+		console.log("This is new player position - " + data["side"]);
+		
+		//sending the new player's data to the other player
+		socket.broadcast.emit("newPlayer", data);
+		
+		//Telling the second player first player's position
+		if(data["player"] == "second")
+			socket.emit("firstPosition", players[0]);
+
+		else if(data["player"] == "third")
+		{
+			socket.emit("firstPosition", players[0]);
+			socket.emit("firstPosition", players[1]);
+		}	
+		else if(data["player"] == "fourth")
+		{
+			socket.emit("firstPosition", players[0]);
+			socket.emit("firstPosition", players[1]);			
+			socket.emit("firstPosition", players[2]);
+		}
+
+	});
+
+	socket.on("playersReady", function(data)
+	{
+		for(var y = 0; y < 929; y + 32 )
+		{
+			for(var x = 0; x < 929; x + 32)
+			{
+				io.sockets.emit("drawMap", {
+								"x":x,
+								"y":y})
+			}
+		}
+		
+		
+		for(var i = 1; i < 51; i++)
+		{
+			goldBit = {
+						"id" : "gold" + i,
+						"sprite" : "gold.png",
+						"x" : (Math.random()*896),
+						"y" : (Math.random()*896)
+					};
+					
+			io.sockets.emit("enterGoldBit", goldBit);
+
+		}	   
+	});
+
+	socket.on('disconnect', function()
+		{
+			var xxx = sckt.indexOf(socket.id);
+			
+			//deleting the player that left
+			if(sckt.indexOf(socket.id) != -1)
+			{
+				sckt.splice(xxx, 1);
+			}
+			
+			//deleting the player that left							
+			for(var tmp in players)
+			{
+				if(players[tmp]["id"] == socket.id)
+				{
+					//Sending the data of the player, that left
+					io.sockets.emit("playerLeft", players[tmp]);
+					
+					players.splice(tmp, 1);
+				}
+				
+				//Logging the players count
+				console.log(players.length);
+			}
+			
+			//Logging the socket array
+			console.log(sckt);	
+		});	
+
+});
 //another project's code for reference purposes
 
 /*var sckt = [];
