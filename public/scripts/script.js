322bbb43d728b6d5e97fb920c3469ea1c0482970
Garend95 <garend95@yahoo.com>
2016-07-23 01:54:47 +0400

    established socket to connect with server (partial), a function that runs once all inGame preparations are done, gives tiles an Id and adds to stage. Tile object c reated and assets to load specified.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,97 @@
+var stage, queue, bg, player, socketPlayer, ball;
+
+//var tileId = Array[][];
+
+$(function()
+	{
+	
+	var socket;
+
+		window["socket"] = socket;				
+
+		stage = new createjs.Stage("my_canvas");
+
+		queue = new createjs.LoadQueue(true);
+		
+		queue.addEventListener("complete", handleComplete);
+		
+		queue.loadManifest([{id:"gold", src:"/public/pics/resource/gold.png"},
+							{id:"goldUp", src:"/public/pics/resource/cargo_gold_2.png"},
+							{id:"goldDown", src:"/public/pics/resource/cargo_gold_4.png"},
+							{id:"goldLeft", src:"/public/pics/resource/cargo_gold_1.png"},
+							{id:"goldRight", src:"/public/pics/resource/cargo_gold_3.png"},
+							{id:"tankRedLeft", src:"/public/pics/resource/player_red_1.png"}
+							{id:"tankRedRight", src:"/public/pics/resource/player_red_3.png"}
+							{id:"tankRedUp", src:"/public/pics/resource/player_red_2.png"}
+							{id:"tankRedDown", src:"/public/pics/resource/player_red_4.png"}
+							{id:"tankBlueLeft", src:"/public/pics/resource/player_blue_1.png"}
+							{id:"tankBlueRight", src:"/public/pics/resource/player_blue_3.png"}
+							{id:"tankBlueUp", src:"/public/pics/resource/player_blue_2.png"}
+							{id:"tankBlueDown", src:"/public/pics/resource/player_blue_4.png"}
+							{id:"tankyellowLeft", src:"/public/pics/resource/player_yellow_1.png"}
+							{id:"tankyellowRight", src:"/public/pics/resource/player_yellow_3.png"}
+							{id:"tankyellowUp", src:"/public/pics/resource/player_yellow_2.png"}
+							{id:"tankyellowDown", src:"/public/pics/resource/player_yellow_4.png"}
+							{id:"tankgreenLeft", src:"/public/pics/resource/player_green_1.png"}
+							{id:"tankgreenRight", src:"/public/pics/resource/player_green_3.png"}
+							{id:"tankgreenUp", src:"/public/pics/resource/player_green_2.png"}
+							{id:"tankgreenDown", src:"/public/pics/resource/player_green_4.png"}
+							{id:"camp1", src:"/public/pics/resource/camp_blue.png"},
+							{id:"camp2", src:"/public/pics/resource/camp_yellow.png"},
+							{id:"camp3", src:"/public/pics/resource/camp_red.png"},
+							{id:"camp4", src:"/public/pics/resource/camp_green.png"},
+							{id:"bulletH", src:"/public/pics/resource/bullet1_1.png"},
+							{id:"bulletV", src:"/public/pics/resource/bullet1_2.png"},
+							{id:"grass", src:"/public/pics/grass.png"},
+							{id:"mars", src:"/public/pics/mars.png"},
+							{id:"moon", src:"/public/pics/moon.png"},
+							{id:"sand", src:"/public/pics/sand.png"}]);
+
+
+		function handleComplete(evt)
+				{
+					var i = 0, j = 0;
+					for(var y = 0; y < 929; y + 32 )
+						{
+							for(var x = 0; x < 929; x + 32)
+							{
+								/*tileId.push(y);
+								tileId[y].push(x);*/
+
+								tile = new Tile(("id" +j) + i, x, y);
+
+								stage.addChild(tile.tileImage);
+
+								i++;
+							}
+							j++;
+						}
+
+
+					socket = io.connect();
+					
+					update();
+				}	
+			
+});
+
+function Tile(id, x, y)
+{
+	this.id = id;
+
+	this.x = x;
+	this.y = y;
+
+	var luck = Math.random();
+
+	if(luck < 0 && luck >= 0.25) this.tileImage = new createjs.Bitmap(queue.getResult("sand"));
+	else if(luck < 0.25 && luck >= 0.50) this.tileImage = new createjs.Bitmap(queue.getResult("mars"));
+	else if(luck < 0.50 && luck >= 0.75) this.tileImage = new createjs.Bitmap(queue.getResult("grass"));
+	else this.tileImage = new createjs.Bitmap(queue.getResult("moon"));
+	
+	this.tileImage.x = this.x;
+	this.tileImage.y = this.y;
+}
 //another project's code for reference purposes
 
 /*var stage, queue, bg, player, socketPlayer, ball;
@@ -99,6 +193,11 @@ $(function()
 				//show everything in the stage
 				stage.update();
 			});
+
+			socket.on("drawMap", function(data)
+			{
+				tile = new 
+			})
 			
 			//waiting for players to leave the game
 			socket.on("playerLeft", function(data)
@@ -216,6 +315,23 @@ function Player(id, sd, wh, x, y)
 	this.playerImage.y = this.y;
 }
 
+function Tile(id, x, y)
+{
+	this.id = id;
+
+	this.x = x;
+	this.y = y;
+
+	var luck = Math.random();
+
+	if(luck < 0 && luck >= 0.25) this.tileImage = new createjs.Bitmap(queue.getResult("sand"));
+	else if(luck < 0.25 && luck >= 0.50) this.tileImage = new createjs.Bitmap(queue.getResult("mars"));
+	else if(luck < 0.50 && luck >= 0.75) this.tileImage = new createjs.Bitmap(queue.getResult("grass"));
+	else this.tileImage = new createjs.Bitmap(queue.getResult("moon"));
+
+	this.tileImage.x = this.x;
+	this.tileImage.y = this.y;
+}
 //creating the ball class locally
 function Ball(id, x, y)
 {
