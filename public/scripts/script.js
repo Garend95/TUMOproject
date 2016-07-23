8d76d47af2c79ce3838dbf084ca1167b69618eb8
Garend95 <garend95@yahoo.com>
2016-07-23 11:42:11 +0400

    player object created, different tank color for each player added

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -67,6 +67,24 @@ $(function()
 							j++;
 						}
 
+		socket.on("yourID", function(data)
+				{
+					//making the side global
+					window["side"] = data["side"];
+					
+					//creating the object
+					player = new Player(data["name"], window["side"], data["player"], data["x"], data["y"]);
+					
+					//adding player to the stage
+					stage.addChild(player.playerImage);
+				
+					//Report the server that player is created, and send the data
+					socket.emit("newPlayerCreated", {"id":player.id, "x":player.x, "y":player.y, "side":player.screenSide, "player":player.who});
+					
+					// show everything in the stage				
+					stage.update();				
+				});
+
 
 					socket = io.connect();
 					
@@ -76,6 +94,36 @@ $(function()
 				
 			
 });
+
+function Player(id, sd, wh, x, y, img)
+{
+	this.id = id;
+	
+	this.screenSide = sd;
+	
+	this.who = wh;
+	
+	this.x = x;
+	this.y = y;
+	
+	switch (img){
+		case 1:
+			this.playerImage = new createjs.Bitmap(queue.getResult("tankRedUp"));
+			break;
+		case 2:
+			this.playerImage = new createjs.Bitmap(queue.getResult("tankBlueUp"));
+			break;
+		case 3:
+			this.playerImage = new createjs.Bitmap(queue.getResult("tankYellowUp"));
+			break;
+		case 4:	
+			this.playerImage = new createjs.Bitmap(queue.getResult("tankGreenUp"));
+			break;	
+	
+	}
+	this.playerImage.x = this.x;
+	this.playerImage.y = this.y;
+}
 
 function Tile(id, x, y)
 {
